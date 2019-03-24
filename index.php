c28e1ca9226fa88b02f8bbcab8482432e132d974
Maxemilian Grönblom <maxemiliang@gmail.com>
2019-03-24 20:23:30 +0200

    Updated with some more examples

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+// to run: php -S 127.0.0.1:8000 and access the endpoints
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1); // DEV ENV
 
@@ -10,3 +13,14 @@ $app->get("/", function () use ($app) {
     $r = array('msg' => 'Hello');
     $app->render("home.php", $r);
 });
+
+$app->get("/hello", function () use ($app) {
+    $r = array('msg' => 'World');
+    $app->render("home.php", $r);
+});
+
+$app->get("/hello/world", function () use ($app) {
+    $r = array('msg' => 'Hello world');
+    $app->render("home.php", $r);
+});
+ 
