9c7a15042b77aeeb816da206a9eebcccc8c3204b
Maxemilian Grönblom <maxemiliang@gmail.com>
2019-03-24 20:20:23 +0200

    Added some examples

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1); // DEV ENV
 
 require "rout/main.php";
 
 $app = new Rout();
 
-print_r($_SERVER);
+$app->get("/", function () use ($app) {
+    $r = array('msg' => 'Hello');
+    $app->render("home.php", $r);
+});
--- rout/login.class.php
+++ rout/login.class.php
@@ -12,114 +12,99 @@ This is the login controller, which suprise suprise, handles logins
 
 */
 
-Class Login extends Rout
+class Login extends Rout
 {
 
-	public function isLoggedIn()
-	{
+  public function isLoggedIn()
+  {
 
-		if (isset($_SESSION["userID"])) {
-			
-			return true;
+    if (isset($_SESSION["userID"])) {
 
-		} else {
+      return true;
+    } else {
 
-			return false;
-
-		}
-
-	}
+      return false;
+    }
+  }
 
 
-	public function getUserId() {
+  public function getUserId()
+  {
 
-		return $_SESSION["userID"];
-
-	}
-
-
-	public function tryLogin($l) // accepts arrays with the structure of ["username"] followed by ["password"]!
-	{
-
-		$q = $this->db->prepare("SELECT * FROM users WHERE username=?;");
-
-		$r = $q->execute(array($l["username"]));
-
-		$v = $q->fetchAll();
-
-		if (count($v) == 1) { 
-
-			if (password_verify($l["password"], $v[0]["password"])) {
-
-				$_SESSION["userID"] = $v[0]["uID"];
-
-				$_SESSION["user"] = $v[0]["username"];
-
-				$_SESSION["priv"] = $v[0]["privileges"];
-
-				return true;
-
-			} else {
-
-				return false;
-
-			}
-
-		} else {
-
-			return false;
-		}
+    return $_SESSION["userID"];
+  }
 
 
-	}
+  public function tryLogin($l) // accepts arrays with the structure of ["username"] followed by ["password"]!
+  {
+
+    $q = $this->db->prepare("SELECT * FROM users WHERE username=?;");
+
+    $r = $q->execute(array($l["username"]));
+
+    $v = $q->fetchAll();
+
+    if (count($v) == 1) {
+
+      if (password_verify($l["password"], $v[0]["password"])) {
+
+        $_SESSION["userID"] = $v[0]["uID"];
+
+        $_SESSION["user"] = $v[0]["username"];
+
+        $_SESSION["priv"] = $v[0]["privileges"];
+
+        return true;
+      } else {
+
+        return false;
+      }
+    } else {
+
+      return false;
+    }
+  }
 
 
-	public function tryLogout() 
-	{
+  public function tryLogout()
+  {
 
-		unset($_SESSION["userID"]);
+    unset($_SESSION["userID"]);
 
-		return true;
+    return true;
+  }
 
-	}
 
+  public function createUser($username, $password, $priv)
+  {
 
-	public function createUser($username, $password, $priv)
-	{
+    $q = $this->db->prepare("SELECT * FROM users WHERE username=?;");
 
-		$q = $this->db->prepare("SELECT * FROM users WHERE username=?;");
+    $r = $q->execute(array($username));
 
-		$r = $q->execute(array($username));
+    $v = $q->fetchAll();
 
-		$v = $q->fetchAll();
+    if (count($v) <= 0) {
 
-		if (count($v) <= 0) {
+      if (strlen($password) >= 8 && strlen($username) >= 5) {
+        $password = password_hash($password, PASSWORD_DEFAULT); //prepare password
 
-			if (strlen($password) >= 8 && strlen($username) >= 5) {
-				$password = password_hash($password, PASSWORD_DEFAULT); //prepare password
+        $sth = $this->db->prepare("INSERT INTO users (username, password, privileges) VALUES (?, ?, ?);");
 
-				$sth = $this->db->prepare("INSERT INTO users (username, password, privileges) VALUES (?, ?, ?);");
+        if ($sth->execute(array($username, $password, $priv))) {
 
-				if($sth->execute(array($username, $password, $priv))) {
+          return 1; // added
+        } else {
 
-					return 1; // added
+          return 3; // error with execute
+        }
+      } else {
 
-				} else {
+        return 2; //  error with length
+      }
+    } else {
 
-					return 3; // error with execute
-
-				}
-			} else {
-
-				return 2; //  error with length
-
-			}
-
-		} else {
-
-			return 4; // error user found 
-
-		}
-	}
-
+      return 4; // error user found 
+    }
+  }
 }
